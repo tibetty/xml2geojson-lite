1edf9a44347276a369d14da73906f8088053c8fa
git <xihua.duan@tourank.com>
2018-12-27 00:19:29 +0800

    bug fixes + refactoring

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 module.exports = (xml) => {
-	// parse outer/inner ways
-	let outerWays = [], outerFirstMap = {}, outerLastMap = {};
-	let innerWays = [], innerFirstMap = {}, innerLastMap = {};
-	
+	let segments = xml.split(/(<member type="way" ref=".+" role=".+">[^]+?<\/member>)/g);
+	let waySegments = [];
+	for (let segment of segments) {
+		if (/<member type="way" ref=".+" role=".+">[^]+?<\/member>/g.test(segment)) {
+			waySegments.push(segment);
+		}
+	}
+
 	const first = a => a[0];
 	const last = a => a[a.length - 1];
 	
@@ -19,134 +23,89 @@ module.exports = (xml) => {
 	
 	function getFromMap(m, k) {
 		let a = m[k];
-		if (a && a.length >= 1) return a[0];
+		if (a && a.length > 0) return a[0];
 		return null;
 	}
 
 	const coordsToKey = (coords) => coords.join(',');
+
+	let outerWays = [], outerFirstMap = {}, outerLastMap = {};
+	let innerWays = [], innerFirstMap = {}, innerLastMap = {};
 	
-	let outerSegments = xml.split(/(<member type="way" ref="\d+" role="outer">)/);
-	for (let i = 1; i < outerSegments.length; i += 2) {
-		let idx = outerSegments[i + 1].indexOf('</member>');
-		let outerContent = outerSegments[i + 1].substring(0, idx);
-		let leftOver = outerSegments[i + 1].substring(idx + '</member>'.length);
-		let way = [];
+	for (let segment of waySegments) {
+		let ways = innerWays, firstMap = innerFirstMap, lastMap = innerLastMap;
+		if (/<member type="way" ref=".+" role="outer">/g.test(segment)) {
+			ways = outerWays, firstMap = outerFirstMap, lastMap = outerLastMap;
+		}
+
 		let ndRegEx = /<nd lat="([\d\.]+)" lon="([\d\.]+)"\/>/g;
-		let match = null;
-		while (match = ndRegEx.exec(outerContent))
+		let match = null, way = [];
+		while (match = ndRegEx.exec(segment))
 			way.push([match[2], match[1]]);
-		outerWays.push(way);
-		addToMap(outerFirstMap, coordsToKey(first(way)), way);
-		addToMap(outerLastMap, coordsToKey(last(way)), way);
-		let innerSegments = leftOver.split(/(<member type="way" ref="\d+" role="inner">)/);
-		if (innerSegments.length > 1) {
-			for (let j = 1; j < innerSegments.length; j += 2) {
-				let innerContent = innerSegments[j + 1].substring(0, innerSegments[j + 1].indexOf('</member>'));
-				let way = [];
-				while (match = ndRegEx.exec(innerContent))
-					way.push([match[2], match[1]]);
-				innerWays.push(way);
-				addToMap(innerFirstMap, coordsToKey(first(way)), way);
-				addToMap(innerLastMap, coordsToKey(last(way)), way);
+		ways.push(way);
+		addToMap(firstMap, coordsToKey(first(way)), way);
+		addToMap(lastMap, coordsToKey(last(way)), way);
+	}
+
+	function constructPolygons(ways, firstMap, lastMap, clockwise) {
+		const isRing = a => coordsToKey(first(a)) === coordsToKey(last(a));
+		const strToFloat = el => el instanceof Array? el.map(strToFloat) : parseFloat(el);
+
+		function isClockwise(a, xIdx, yIdx) {
+			xIdx = xIdx || 0, yIdx = yIdx || 1;
+			let m = a.reduce((last, v, current) => a[last][0] > v[0] ? last : current, 0);
+			let l = m <= 0? a.length - 1 : m - 1, r = m >= a.length - 1? 0 : m + 1;
+			let xa = a[l][xIdx], xb = a[m][xIdx], xc = a[r][xIdx];
+			let ya = a[l][yIdx], yb = a[m][yIdx], yc = a[r][yIdx];
+			let det = (xb - xa) * (yc - ya) - (xc - xa) * (yb - ya);
+			return det < 0;
+		}
+
+		let rs = [], way = null;
+		while (way = ways.pop()) {
+			removeFromMap(firstMap, coordsToKey(first(way)), way);
+			removeFromMap(lastMap, coordsToKey(last(way)), way);
+
+			if (isRing(way)) {
+				way = strToFloat(way);
+				if (isClockwise(way) !== clockwise) way.reverse();
+				rs.push(way);
+			} else {
+				let line = [];
+				let current = way;
+				let reversed = false;
+				while (current) {
+					line = line.concat(current);
+					let key = coordsToKey(last(line));
+					reversed = false;
+
+					current = getFromMap(firstMap, key);										
+					if (!current) {
+						current = getFromMap(lastMap, key);
+						reversed = true;
+					}
+					
+					if (current) {
+						ways.splice(ways.indexOf(current), 1);
+						removeFromMap(firstMap, coordsToKey(first(current)), current);
+						removeFromMap(lastMap, coordsToKey(last(current)), current);
+						if (reversed) current.reverse();
+						current = current.slice(1);
+					}
+				}
+				// points of an outerpolygon should be organized counterclockwise
+				if (isRing(line)) {
+					line = strToFloat(line);
+					if (isClockwise(line) !== clockwise) line.reverse();
+					rs.push(line);
+				}
 			}
 		}
-	}
-	
-	const isRing = a => coordsToKey(first(a)) === coordsToKey(last(a));
-	
-	function isClockwise(a, xIdx, yIdx) {
-		xIdx = xIdx || 0, yIdx = yIdx || 1;
-		let m = a.reduce((last, v, current) => a[last][0] > v[0] ? last : current, 0);
-		let l = m <= 0? a.length - 1 : m - 1, r = m >= a.length - 1? 0 : m + 1;
-		let xa = a[l][xIdx], xb = a[m][xIdx], xc = a[r][xIdx];
-		let ya = a[l][yIdx], yb = a[m][yIdx], yc = a[r][yIdx];
-		let det = (xb - xa) * (yc - ya) - (xc - xa) * (yb - ya);
-		return det < 0;
+		return rs;
 	}
 
-	const strToFloat = el => el instanceof Array? el.map(strToFloat) : parseFloat(el);
-
-	// join outer ways to form outer polygons
-	let outerPolygons = [], innerPolygons = [];
-	let way = null;
-	while (way = outerWays.pop()) {
-		if (isRing(way)) {
-			way = strToFloat(way);
-			if (isClockwise(way)) way.reverse();
-			outerPolygons.push(way);
-		} else {
-			let line = [];
-			let current = way;
-			let reversed = false;
-			removeFromMap(outerFirstMap, coordsToKey(first(current)), current);
-			removeFromMap(outerLastMap, coordsToKey(last(current)), current);
-			while (current) {
-				line = line.concat(current);
-				let key = coordsToKey(last(line));
-				reversed = false;
-
-				current = getFromMap(outerFirstMap, key);										
-				if (!current) {
-					current = getFromMap(outerLastMap, key);
-					reversed = true;
-				}
-				
-				if (current) {
-					outerWays.splice(outerWays.indexOf(current), 1);
-					removeFromMap(outerFirstMap, coordsToKey(first(current)), current);
-					removeFromMap(outerLastMap, coordsToKey(last(current)), current);
-					if (reversed) current.reverse();
-					current = current.slice(1);
-				}
-			}
-			// points of an outerpolygon should be organized counterclockwise
-			if (isRing(line)) {
-				line = strToFloat(line);
-				if (isClockwise(line)) line.reverse();
-				outerPolygons.push(line);
-			}
-		}
-	}
-	
-	// join inner ways to form outer polygons
-	while (way = innerWays.pop()) {
-		if (isRing(way)) {
-			way = strToFloat(way);
-			if (!isClockwise(way)) way.reverse();
-			innerPolygons.push(way);
-		} else {
-			let line = [];
-			let current = way;
-			let reversed = false;
-			removeFromMap(innerFirstMap, coordsToKey(first(current)), current);
-			removeFromMap(innerLastMap, coordsToKey(last(current)), current);
-			while (current) {
-				line = line.concat(current);
-				let key = coordsToKey(last(line));
-				reversed = false;
-
-				current = getFromMap(innerFirstMap, key);
-				if (!current) {
-					current = getFromMap(innerLastMap, key);
-					reversed = true;
-				}
-
-				if (current) {
-					innerWays.splice(innerWays.indexOf(current), 1);
-					removeFromMap(innerFirstMap, coordsToKey(first(current)), current);
-					removeFromMap(innerLastMap, coordsToKey((current)), current);
-					if (reversed) current.reverse();
-					current = current.slice(1);
-				}
-			}
-			// points of an innerpolygon should be organized clockwise
-			if (isRing(line)) {
-				line = strToFloat(line);
-				if (!isClockwise(line)) line.reverse();
-				innerPolygons.push(line);
-			}
-		}
-	}
+	let outerPolygons = constructPolygons(outerWays, outerFirstMap, outerLastMap, false),
+		innerPolygons = constructPolygons(innerWays, innerFirstMap, innerLastMap, true);
 	
 	// link inner polygons to outer containers
 	function ptInsidePolygon(pt, polygon, lngIdx, latIdx) {
@@ -162,6 +121,7 @@ module.exports = (xml) => {
 	}
 	
 	let compositPolyons = [];
+
 	for (let idx in outerPolygons) {
 		compositPolyons[idx] = [];
 		compositPolyons[idx].push(outerPolygons[idx]);
@@ -176,7 +136,7 @@ module.exports = (xml) => {
 			}
 		}
 	}
-	
+		
 	// construct return value (geojson polyon or multipolygon)
 	let geom = {
 		type: 'MultiPolygon',
@@ -190,4 +150,3 @@ module.exports = (xml) => {
 	
 	return geom;
 };
-
